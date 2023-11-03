a0f9c3f61b87ac62da2f106759a6bb3c55d8639b
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-11-03 14:34:14 +0100

    adding Nickname

--- BlazorQuizWASM/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BlazorQuizWASM/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,10 @@ namespace BlazorQuizWASM.Server.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [StringLength(15, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
+            [Display(Name = "Nickname")]
+            public string Nickname { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -117,6 +121,8 @@ namespace BlazorQuizWASM.Server.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.Nickname = Input.Nickname;
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)

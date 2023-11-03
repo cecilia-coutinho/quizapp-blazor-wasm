760b87b33c2cff479d3e867e16f29b026db902cf
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-11-03 14:13:54 +0100

    typo correction

--- BlazorQuizWASM/Server/Models/Domain/Question.cs
+++ BlazorQuizWASM/Server/Models/Domain/Question.cs
@@ -13,7 +13,7 @@ namespace BlazorQuizWASM.Server.Models.Domain
         public Guid FkUserId { get; set; }
 
         [Required]
-        public Guid FkFilesId { get; set; }
+        public Guid FkFileId { get; set; }
 
         [NotNull]
         public string? Title { get; set; }
@@ -25,7 +25,7 @@ namespace BlazorQuizWASM.Server.Models.Domain
         [ForeignKey("FkUserId")]
         public virtual ApplicationUser? ApplicationUsers { get; set; }
 
-        [ForeignKey("FkFilesId")]
+        [ForeignKey("FkFileId")]
         public virtual MediaFile? MediaFiles { get; set; }
         public List<Answer>? Answers { get; set; }
         public List<QuizItem>? QuizItems { get; set; }

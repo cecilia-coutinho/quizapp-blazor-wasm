909cc40cbb9848bd930fba511f76b879488f4d68
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-11-08 20:42:10 +0100

    repository update

--- BlazorQuizWASM/Shared/DTO/QuestionRequestDto.cs
+++ BlazorQuizWASM/Shared/DTO/QuestionRequestDto.cs
@@ -1,29 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlazorQuizWASM.Shared.DTO
 {
     public class QuestionRequestDto
     {
-        //[Key]
-        //public Guid QuestionId { get; set; }
-
-        [Required]
-        public string? FkUserId { get; set; }
-
-        [Required]
-        public Guid FkFileId { get; set; }
+        [NotNull]
+        public string? MediaFileName { get; set; }
 
         [NotNull]
         public string? Title { get; set; }
 
-        public string? Description { get; set; }
+        [NotNull]
+        public string? Content { get; set; }
 
-
+        [NotNull]
+        public bool IsCorrect { get; set; } = true;
     }
 }

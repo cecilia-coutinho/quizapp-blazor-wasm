dfd97d5b0a0aaccd393d54341d4318dbda3d14fd
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-11-09 16:51:33 +0100

    Interface implementation

---
+++ BlazorQuizWASM/Server/Repositories/IQuizItemRepository.cs
@@ -0,0 +1,10 @@
+﻿using BlazorQuizWASM.Server.Models.Domain;
+
+namespace BlazorQuizWASM.Server.Repositories
+{
+    public interface IQuizItemRepository
+    {
+        Task<QuizItem> CreateAsync(QuizItem quizItem);
+        Task<List<QuizItem>> GetParticipantsPerQuestionAsync(Guid fkQuestionId);
+    }
+}

bc39c7693b05ce370635df95f808d0b7030d4baf
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-11-09 01:50:58 +0100

    IAnswerRepository implementation

---
+++ BlazorQuizWASM/Server/Repositories/IAnswerRepository.cs
@@ -0,0 +1,11 @@
+﻿using BlazorQuizWASM.Server.Models.Domain;
+
+namespace BlazorQuizWASM.Server.Repositories
+{
+    public interface IAnswerRepository
+    {
+        Task<Answer> CreateAsync(Answer answer);
+        Task<List<Answer>> GetAllAsync(string? filterOn = null, string? filterQuery = null, string? sortBy = null, bool isAscending = true, int pageNumber = 1, int pageSize = 1000);
+        Task<Answer?> DeleteAsync(Guid id);
+    }
+}

c1ae342d8f1e25bee5d59c00cc4b93978c2c69e2
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-11-13 09:54:41 +0100

    dto added

---
+++ BlazorQuizWASM/Shared/DTO/QuizItemResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace BlazorQuizWASM.Shared.DTO
+{
+    public class QuizItemResponseDto
+    {
+        [NotNull]
+        public string? Nickname { get; set; }
+
+        [NotNull]
+        public bool IsScored { get; set; }
+
+        [NotNull]
+        public int TimeLimit { get; set; }
+
+        [NotNull]
+        public DateTime Started_At { get; set; }
+    }
+}

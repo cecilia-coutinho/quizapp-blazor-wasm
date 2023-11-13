e796a6a6842ca2480a494a53cba83f34adccc07f
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-11-13 09:51:30 +0100

    task GetScore added

--- BlazorQuizWASM/Server/Repositories/IQuizItemRepository.cs
+++ BlazorQuizWASM/Server/Repositories/IQuizItemRepository.cs
@@ -6,6 +6,7 @@ namespace BlazorQuizWASM.Server.Repositories
     public interface IQuizItemRepository
     {
         Task<QuizItem> CreateAsync(QuizItem quizItem);
-        Task<List<QuizItemRequestDto>> GetParticipantsPerQuestionAsync(Guid fkQuestionId);
+        Task<List<QuizItemResponseDto>> GetParticipantsPerQuestionAsync(Guid fkQuestionId);
+        Task<List<QuizItem>> GetScore(string fkUserId);
     }
 }

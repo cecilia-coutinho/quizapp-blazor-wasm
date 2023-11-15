e906ab775d54563746c0cd304fe829d79f517bad
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-11-15 22:22:10 +0100

    file create

---
+++ BlazorQuizWASM/Shared/DTO/UploadMediaResultDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorQuizWASM.Shared.DTO
+{
+    public class UploadMediaResultDto
+    {
+            public string? FileName { get; set; }
+            public string? StoredFileName { get; set; }
+    }
+}

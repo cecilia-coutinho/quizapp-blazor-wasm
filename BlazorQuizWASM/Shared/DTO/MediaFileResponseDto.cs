a0019bd539060e3acb3191bde3c26d6928a59a0f
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-11-16 01:13:08 +0100

    properties update

--- BlazorQuizWASM/Shared/DTO/MediaFileResponseDto.cs
+++ BlazorQuizWASM/Shared/DTO/MediaFileResponseDto.cs
@@ -6,11 +6,7 @@ namespace BlazorQuizWASM.Shared.DTO
     {
         [NotNull]
         public string? MediaFileName { get; set; }
-
         public string? StoredFileName { get; set; }
-        public string? FileExtension { get; set; }
-        public long FileSizeInBytes { get; set; }
-        public string? FilePath { get; set; }
         public bool Uploaded { get; set; }
         public int ErrorCode { get; set; }
     }

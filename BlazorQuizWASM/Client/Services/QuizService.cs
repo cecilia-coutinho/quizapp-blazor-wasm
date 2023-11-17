3e00653387e2db1f4137ed0ce49934f3cde46a90
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-11-17 19:42:41 +0100

    token request service

---
+++ BlazorQuizWASM/Client/Services/QuizService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using System.Net.Http.Headers;
+
+namespace BlazorQuizWASM.Client.Services
+{
+    public class QuizService
+    {
+        private readonly IAccessTokenProvider _tokenProvider;
+        private readonly HttpClient _httpClient;
+
+        public QuizService(IAccessTokenProvider tokenProvider, HttpClient httpClient)
+        {
+            _tokenProvider = tokenProvider;
+            _httpClient = httpClient;
+        }
+
+        public async Task RequestAuthToken()
+        {
+            var requestToken = await _tokenProvider.RequestAccessToken();
+            requestToken.TryGetToken(out var token);
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value);
+        }
+
+    }
+
+}

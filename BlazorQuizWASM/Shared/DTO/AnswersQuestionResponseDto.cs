d19d38aec99de147d5989fbbcc333ad697478009
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-11-21 21:51:45 +0100

    dto create

---
+++ BlazorQuizWASM/Shared/DTO/AnswersQuestionResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorQuizWASM.Shared.DTO
+{
+    public class AnswersQuestionResponseDto
+    {
+        public QuestionRequestDto? Question { get; set; }
+        public List<AnswerRequestDto>? Answers { get; set; }
+    }
+}

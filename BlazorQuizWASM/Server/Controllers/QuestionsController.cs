a298e9ae86fbfa403a78085e2571843e93a4176f
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-11-22 15:06:57 +0100

    refactored

--- BlazorQuizWASM/Server/Controllers/QuestionsController.cs
+++ BlazorQuizWASM/Server/Controllers/QuestionsController.cs
@@ -9,7 +9,7 @@ namespace BlazorQuizWASM.Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class QuestionsController : ControllerBase
     {
         private readonly IQuestionRepository _questionRepository;
@@ -64,70 +64,108 @@ namespace BlazorQuizWASM.Server.Controllers
             });
         }
 
-        // GET questions
-        // GET: api/Questions/
-        [HttpGet]
-        public async Task<ActionResult> GetAllPublished(
-            [FromQuery] string? filterOn,
-            [FromQuery] string? filterQuery,
-            [FromQuery] string? sortBy, [FromQuery] bool? isAscending,
-            [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 1000
-            )
+        // UPDATE question to publish
+        // PUT: api/Questions/question/{id}
+        [HttpPut("question/{questionPath}")]
+        public async Task<IActionResult> Update(string questionPath)
         {
-            var questionDomainModel = await _questionRepository
-                .GetAllAsync(
-                filterOn,
-                filterQuery,
-                sortBy,
-                isAscending ?? true,
-                pageNumber,
-                pageSize);
-
-            var questions = questionDomainModel
-                .Where(q => q.IsPublished == true)
-                .Select(q => q.Title)
-                .ToList();
-
-            return Ok(new { Question = questions });
-        }
-
-
-
-        // GET questions
-        // GET: api/Questions/questions-by-user
-        [HttpGet]
-        [Route("questions-by-user")]
-        public async Task<ActionResult> GetAllByUser(
-            [FromQuery] string? filterOn,
-            [FromQuery] string? filterQuery,
-            [FromQuery] string? sortBy, [FromQuery] bool? isAscending,
-            [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 1000
-            )
-        {
-
             var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-            var questionDomainModel = await _questionRepository
-                .GetAllAsync(
-                filterOn,
-                filterQuery,
-                sortBy,
-                isAscending ?? true,
-                pageNumber,
-                pageSize);
+            //get question Id
+            var requestedQuestion = await _questionRepository.GetQuestionByPath(questionPath);
+            var questionId = requestedQuestion.QuestionId;
 
-            var questions = questionDomainModel
-            .Where(q => q.FkUserId == userId)
-            .Select(q => new
+            Question question = new()
             {
-                Title = q.Title,
-                IsPublished = q.IsPublished,
-                QuestionPath = q.QuestionPath
-            })
-            .ToList();
+                Title = requestedQuestion.Title,
+                FkUserId = userId,
+                FkFileId = requestedQuestion.FkFileId,
+                QuestionPath = requestedQuestion.QuestionPath,
+                TimeLimit = requestedQuestion.TimeLimit,
+                IsPublished = true
+            };
 
-            return Ok(new { Question = questions });
+            var questionDomainModel = await _questionRepository.UpdateAsync(questionId, question);
+
+            if (questionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            var updatedQuestionDto = new QuestionRequestDto
+            {
+                Title = questionDomainModel.Title,
+                QuestionPath = questionDomainModel.QuestionPath,
+                IsPublished = questionDomainModel.IsPublished
+            };
+
+            return Ok(updatedQuestionDto);
         }
+
+        //// GET questions
+        //// GET: api/Questions/
+        //[HttpGet]
+        //public async Task<ActionResult> GetAllPublished(
+        //    [FromQuery] string? filterOn,
+        //    [FromQuery] string? filterQuery,
+        //    [FromQuery] string? sortBy, [FromQuery] bool? isAscending,
+        //    [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 1000
+        //    )
+        //{
+        //    var questionDomainModel = await _questionRepository
+        //        .GetAllAsync(
+        //        filterOn,
+        //        filterQuery,
+        //        sortBy,
+        //        isAscending ?? true,
+        //        pageNumber,
+        //        pageSize);
+
+        //    var questions = questionDomainModel
+        //        .Where(q => q.IsPublished == true)
+        //        .Select(q => q.Title)
+        //        .ToList();
+
+        //    return Ok(new { Question = questions });
+        //}
+
+
+
+        //// GET questions
+        //// GET: api/Questions/questions-by-user
+        //[HttpGet]
+        //[Route("questions-by-user")]
+        //public async Task<ActionResult> GetAllByUser(
+        //    [FromQuery] string? filterOn,
+        //    [FromQuery] string? filterQuery,
+        //    [FromQuery] string? sortBy, [FromQuery] bool? isAscending,
+        //    [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 1000
+        //    )
+        //{
+
+        //    var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        //    var questionDomainModel = await _questionRepository
+        //        .GetAllAsync(
+        //        filterOn,
+        //        filterQuery,
+        //        sortBy,
+        //        isAscending ?? true,
+        //        pageNumber,
+        //        pageSize);
+
+        //    var questions = questionDomainModel
+        //    .Where(q => q.FkUserId == userId)
+        //    .Select(q => new
+        //    {
+        //        Title = q.Title,
+        //        IsPublished = q.IsPublished,
+        //        QuestionPath = q.QuestionPath
+        //    })
+        //    .ToList();
+
+        //    return Ok(new { Question = questions });
+        //}
 
 
         // GET questions with answers
@@ -191,80 +229,80 @@ namespace BlazorQuizWASM.Server.Controllers
 
         // GET questions by Id
         // GET: api/Questions/question/{questionPath}
-        [HttpGet("question/{questionPath}")]
-        public async Task<ActionResult> GetQuestionWithAnswers(string questionPath)
-        {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        //[HttpGet("question/{questionPath}")]
+        //public async Task<ActionResult> GetQuestionWithAnswers(string questionPath)
+        //{
+        //    var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-            var question = await _questionRepository.GetQuestionByPathAndUserAsync(questionPath, userId);
-            var questionId = question.QuestionId;
+        //    var question = await _questionRepository.GetQuestionByPathAndUserAsync(questionPath, userId);
+        //    var questionId = question.QuestionId;
 
-            var answers = await _answerRepository.GetAnswerToQuestionAsync(questionId);
-            var response = answers.Select(a => new { a.Content, a.IsCorrect });
+        //    var answers = await _answerRepository.GetAnswerToQuestionAsync(questionId);
+        //    var response = answers.Select(a => new { a.Content, a.IsCorrect });
 
-            var questionDomainModel = await _questionRepository.GetByIdAsync(questionId);
+        //    var questionDomainModel = await _questionRepository.GetByIdAsync(questionId);
 
-            if (questionDomainModel == null)
-            {
-                return NotFound();
-            }
+        //    if (questionDomainModel == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var questionTitle = questionDomainModel.Title;
+        //    var questionTitle = questionDomainModel.Title;
 
-            return Ok(new { Question = questionTitle });
-        }
+        //    return Ok(new { Question = questionTitle });
+        //}
 
         // UPDATE question By Id
         // PUT: api/Questions/question/{id}
-        [HttpPut("question/{questionPath}")]
-        public async Task<IActionResult> Update(string questionPath, [FromForm] QuestionRequestDto questionRequestDto)
-        {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        //[HttpPut("question/{questionPath}")]
+        //public async Task<IActionResult> Update(string questionPath, [FromForm] QuestionRequestDto questionRequestDto)
+        //{
+        //    var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-            //get question Id
-            var requestedQuestion = await _questionRepository.GetQuestionByPath(questionPath);
-            var questionId = requestedQuestion.QuestionId;
+        //    //get question Id
+        //    var requestedQuestion = await _questionRepository.GetQuestionByPath(questionPath);
+        //    var questionId = requestedQuestion.QuestionId;
 
-            // Get uploaded media file
-            var mediaEntity = await _mediaFileRepository.GetMedia(questionRequestDto.MediaFileName);
-            if (mediaEntity == null)
-            {
-                return Problem("Media Entity not found", statusCode: 500);
-            }
+        //    // Get uploaded media file
+        //    var mediaEntity = await _mediaFileRepository.GetMedia(questionRequestDto.MediaFileName);
+        //    if (mediaEntity == null)
+        //    {
+        //        return Problem("Media Entity not found", statusCode: 500);
+        //    }
 
-            Question question = new()
-            {
-                Title = questionRequestDto.Title,
-                FkUserId = userId,
-                FkFileId = mediaEntity.MediaFileId,
-                QuestionPath = questionRequestDto.QuestionPath,
-                TimeLimit = questionRequestDto.TimeLimit,
-                IsPublished = questionRequestDto.IsPublished
-            };
+        //    Question question = new()
+        //    {
+        //        Title = questionRequestDto.Title,
+        //        FkUserId = userId,
+        //        FkFileId = mediaEntity.MediaFileId,
+        //        QuestionPath = questionRequestDto.QuestionPath,
+        //        TimeLimit = questionRequestDto.TimeLimit,
+        //        IsPublished = questionRequestDto.IsPublished
+        //    };
 
-            var questionDomainModel = await _questionRepository.UpdateAsync(questionId, question);
+        //    var questionDomainModel = await _questionRepository.UpdateAsync(questionId, question);
 
-            if (questionDomainModel == null)
-            {
-                return NotFound();
-            }
+        //    if (questionDomainModel == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var updatedQuestionDto = new QuestionRequestDto
-            {
-                Title = questionRequestDto.Title,
-                MediaFileName = questionRequestDto.MediaFileName,
-                QuestionPath = questionRequestDto.QuestionPath,
-                TimeLimit = questionRequestDto.TimeLimit,
-                IsPublished = questionRequestDto.IsPublished
-            };
+        //    var updatedQuestionDto = new QuestionRequestDto
+        //    {
+        //        Title = questionRequestDto.Title,
+        //        MediaFileName = questionRequestDto.MediaFileName,
+        //        QuestionPath = questionRequestDto.QuestionPath,
+        //        TimeLimit = questionRequestDto.TimeLimit,
+        //        IsPublished = questionRequestDto.IsPublished
+        //    };
 
-            return Ok(updatedQuestionDto);
-        }
+        //    return Ok(updatedQuestionDto);
+        //}
 
         // DELETE question By Id
         // DELETE: api/Questions/question/{id}
         [HttpDelete("question/{questionPath}")]
-        public async Task<IActionResult> Delete([FromRoute] string questionPath)
+        public async Task<IActionResult> Delete(string questionPath)
         {
             //get question Id
             var requestedQuestion = await _questionRepository.GetQuestionByPath(questionPath);
